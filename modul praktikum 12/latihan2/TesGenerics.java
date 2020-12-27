826113e1e7bec3fc3420143c5c7f8389820b5a19
Sopia Safira <sofiaxiomi2001@gmail.com>
2020-12-27 16:09:38 +0700

    ANNOTATION, GENERICS, LAMBDA EXPRESSION, METHOD REFERENCE

---
+++ modul praktikum 12/latihan2/TesGenerics.java
@@ -0,0 +1,19 @@
+import java.util.*;  
+
+class TestGenerics{  
+  public static void main(String args[]){  
+    ArrayList<String> list=new ArrayList<String>();  
+    list.add("rani");  
+    list.add("rijal");  
+    
+    //list.add(32);  //compile time error  
+    
+    String s=list.get(1); //tidak perlu melakukan casting 
+    System.out.println("element is: "+s);  
+    
+    Iterator<String> itr=list.iterator();  
+    while(itr.hasNext()){  
+      System.out.println(itr.next());  
+    }    
+  }  
+}
